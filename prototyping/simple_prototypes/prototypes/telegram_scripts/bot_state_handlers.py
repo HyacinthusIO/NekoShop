4b51ef2b2bd04bba63626a9f1d15b85053516a81
HyacinthusIO <oldcat.street34@outlook.com>
2024-06-25 16:00:54 +0900

    The ground has been prepared for the new functionality.
    What the commit adds:
    - New `bot_state_handlers.py`
    - New `test_bot_state_handlers.py`
    
    Other changes:
    - Moved `get_bot_token` from `test_bot_handler` to the new dir `auxiliary_modules/get_bot_token.py`

---
+++ prototyping/simple_prototypes/prototypes/telegram_scripts/bot_state_handlers.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Описание модуля
+
+Copyright 2024 HyacinthusIO
+Лицензия Apache, версия 2.0 (Apache-2.0 license)
+"""
+
+__all__: list[str] = []
+
+__author__ = "HyacinthusIO"
+__version__ = "0.0.0"
---
+++ prototyping/simple_prototypes/tests/tests_telegram_scripts/test_bot_state_handlers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Описание модуля
+
+Copyright 2024 HyacinthusIO
+Лицензия Apache, версия 2.0 (Apache-2.0 license)
+"""
+
+__all__: list[str] = []
+
+__author__ = "HyacinthusIO"
+__version__ = "0.0.0"
+
+import unittest
+import os
+
+PATH_TO_TEST_DIR: str = os.path.dirname(__file__)
+TEST_DATA_DIR_NAME: str = "test_data"
+ENV_FILE_NAME_WITH_CORRECT_BOT_TOKEN: str = "correct_bot_token.env"
