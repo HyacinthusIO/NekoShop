99b006495d9bbbff72aab1c1b00aa7e254013e41
HyacinthusIO <oldcat.street34@outlook.com>
2024-07-26 17:23:17 +0900

    Work on the API and finalise the overall structure.
    Refined the structure and tried to implement well the database view and API.
    Edited the database view and started API development.
    
    Changes:
    - New `interface_database_api.py`;
    - New `mysql_database_api.py`
    - Rename `database_interface.py` -> `abstract_database.py`;
    - Edit `abstract_database.py`
    - Edit `mysql_database.py`

--- prototyping/database_prototypes/database_api/abstract_database.py
+++ prototyping/database_prototypes/database_api/abstract_database.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 
 """
-Модуль `database_interface` используется для предоставления абстрактного класса,
-представляющего общий интерфейс для представления базы данных.
+Модуль `abstract_database` используется для предоставления абстрактного класса,
+представляющего собой базу данных.
 
 Copyright 2024 HyacinthusIO
 Лицензия Apache, версия 2.0 (Apache-2.0 license)
 """
 
-__all__: list[str] = ["DataBaseInterface"]
+__all__: list[str] = ["AbstractDataBase"]
 
 __author__ = "HyacinthusIO"
-__version__ = "0.6.0"
+__version__ = "0.7.1"
 
 from abc import ABC, abstractmethod
+
+from .interface_database_api import Interface_DataBaseAPI
 from typing import Any, Callable, Dict
 
 
 # _____________________________________________________________________________
-class DataBaseInterface(ABC):
-    """DataBaseInterface интерфейс представляющий БД.
+class AbstractDataBase(ABC):
+    """AbstractDataBase класс представляющий собой БД.
 
     Этот класс предоставляет основу для реализации конкретных классов,
     которые будут поддерживать подключение к различным типам баз данных.
@@ -27,23 +29,27 @@ class DataBaseInterface(ABC):
     в производных/дочерних классах под определённый тип БД.
 
     Args:
-        ABC (_type_): базовый/родительский класс.
+        ABC (_type_): Базовый/родительский класс, позволяющий абстракцию.
 
     Attributes:
-        _connection_data (Dict[str, Any]): Данные для аутентификации при подключении к БД.
+        _connection_data (Dict[str, Any]): Данные для создания подключения к БД.
         _connect_method (Callable[..., Any]): Функция, используемая для установки соединения к БД.
         _connection_with_database (Any): Объект, представляющий текущее соединение к БД.
+        api (Interface_DataBaseAPI): Объект, реализующий API интерфейс для определённого типа БД.
     """
 
     _connection_data: Dict[str, Any]
     _connect_method: Callable[..., Any]
     _connection_with_database: Any
+    api: Interface_DataBaseAPI
 
     # -------------------------------------------------------------------------
     def __init__(
         self,
         connect_method: Callable[..., Any],
-        connection_data: Dict[str, str]) -> None:
+        connection_data: Dict[str, str],
+        api: Interface_DataBaseAPI,
+    ) -> None:
         """__init__ конструктор класса.
 
         При инициализации экземпляра класса,
@@ -52,9 +58,11 @@ class DataBaseInterface(ABC):
         Args:
             connect_method (Callable[..., Any]): Функция, используемая для подключения к БД.
             connection_data (Dict[str, str]): Данные (ключевые аргументы), для аутентификации при подключении.
+            api (Interface_DataBaseAPI): Объект, реализующий API интерфейс для определённого типа БД.
         """
         self._connect_method = connect_method
         self._connection_data = connection_data
+        self.api = api
 
         self.create_connection_with_database()
 
@@ -74,7 +82,7 @@ class DataBaseInterface(ABC):
     # -------------------------------------------------------------------------
     @abstractmethod
     def create_connection_with_database(self) -> None:
-        """create_connection установка соединения к БД.
+        """create_connection_with_database установка соединения к БД.
 
         Данный метод должен устанавливать соединение к БД.
         Используя метод и данные для подключения.
@@ -84,8 +92,8 @@ class DataBaseInterface(ABC):
 
     # -------------------------------------------------------------------------
     @abstractmethod
-    async def get_connection_with_database(self) -> Any:
-        """get_connection возвращает объект подключения к БД.
+    def get_connection_with_database(self) -> Any:
+        """get_connection_with_database возвращает объект подключения к БД.
 
         Данный метод должен возвращать объект подключения,
         который будет обеспечивать взаимосвязь класса и БД.
@@ -97,9 +105,19 @@ class DataBaseInterface(ABC):
 
     # -------------------------------------------------------------------------
     @abstractmethod
-    async def close_connection_with_database(self) -> None:
-        """get_connection закрывает подключение к БД.
+    def close_connection_with_database(self) -> None:
+        """close_connection_with_database закрывает подключение к БД.
 
         Данный метод должен закрывать текущее подключение к БД.
+        """
+        pass
+
+    # -------------------------------------------------------------------------
+    @abstractmethod
+    async def connect_api_to_database(self) -> None:
+        """connect_api_to_database устанавливает соединение API к БД.
+
+        Данный метод, должен настроить соединение закреплённого API.
+        Путём передачи активного соединения и вызова соответствующего метода API.
         """
         pass
--- prototyping/database_prototypes/database_api/mysql_database.py
+++ prototyping/database_prototypes/database_api/mysql_database.py
@@ -8,44 +8,59 @@ Copyright 2024 HyacinthusIO
 Лицензия Apache, версия 2.0 (Apache-2.0 license)
 """
 
-__all__: list[str] = ["DataBaseMySQL"]
+__all__: list[str] = ["MySQLDataBase"]
 
 __author__ = "HyacinthusIO"
-__version__ = "0.4.0"
+__version__ = "0.5.1"
 
-from database_api.database_interface import DataBaseInterface
+from .abstract_database import AbstractDataBase
 
 from typing import Dict
 from mysql.connector.connection_cext import CMySQLConnection
-from database_api.custom_types import *
+from .custom_types import *
+from .mysql_database_api import MySQLAPI
 
 
 # _____________________________________________________________________________
-class DataBaseMySQL(DataBaseInterface):
+class MySQLDataBase(AbstractDataBase):
     def __init__(
         self,
         connect_method: MySQLConnectMethod,
-        connection_data: Dict[str, str]) -> None:
-        super().__init__(connect_method=connect_method, 
-                         connection_data=connection_data)
-
-    # -------------------------------------------------------------------------
-    def create_connection_with_database(self) -> None:
-        connect_method: MySQLConnectMethod = self.get_connect_method()
-        self._connection_with_database = connect_method(**self._connection_data)
+        connection_data: Dict[str, str],
+        api: MySQLAPI,
+    ) -> None:
+        super().__init__(
+            connect_method=connect_method,
+            connection_data=connection_data,
+            api=api,
+        )
 
     # -------------------------------------------------------------------------
     def get_connect_method(self) -> MySQLConnectMethod:
         return self._connect_method
 
     # -------------------------------------------------------------------------
-    async def close_connection_with_database(self) -> None:
-        connection: CMySQLConnection = await self.get_connection_with_database()
-        connection.close()
+    def create_connection_with_database(self) -> None:
+        connect_method: MySQLConnectMethod = self.get_connect_method()
+        connection: MySQLConnection = connect_method(**self._connection_data)
+
+        self._connection_with_database = connection
 
     # -------------------------------------------------------------------------
-    async def get_connection_with_database(self) -> CMySQLConnection:
+    def get_connection_with_database(self) -> CMySQLConnection:
         if self._connection_with_database is None:
             self.create_connection_with_database()
 
         return self._connection_with_database
+
+    # -------------------------------------------------------------------------
+    def close_connection_with_database(self) -> None:
+        connection: CMySQLConnection = self.get_connection_with_database()
+
+        connection.close()
+
+    # -------------------------------------------------------------------------
+    async def connect_api_to_database(self) -> None:
+        connection: CMySQLConnection = self.get_connection_with_database()
+
+        await self.api.set_connection_with_database(connection=connection)
